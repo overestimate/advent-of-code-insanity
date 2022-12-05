b26540ef8263ce8dc90fc6bd548a34336358e15d
Emily L <58041173+overestimate@users.noreply.github.com>
2022-12-05 15:21:04 -0600

    add cleaner unminifed day4 + minifed day4

--- day4/day4.py
+++ day4/day4.py
@@ -1,1 +1,5 @@
-print("TODO: add minified code.")
+with open('day4.txt') as f: data = [[[int(z) for z in y.split('-')] for y in x.strip().split(',')] for x in f.readlines()]
+range_groups = [sorted([range(pairing_group[0][0], pairing_group[0][1]+1), range(pairing_group[1][0], pairing_group[1][1]+1)], key=len, reverse=True) for pairing_group in data]
+subset_groups = [g for g in range_groups if g[0][0] <= g[1][0] and g[0][-1] >= g[1][-1]]
+overlap_groups = [g for g in range_groups if True in [True for x in g[1] if x in g[0]]]
+print(len(subset_groups), len(overlap_groups))
--- day4/day4.unminified.py
+++ day4/day4.unminified.py
@@ -1,39 +1,24 @@
 with open('day4.txt') as f: data = [[[int(z) for z in y.split('-')] for y in x.strip().split(',')] for x in f.readlines()]
 running_subset_total = 0
 running_overlap_total = 0
-for i, pairing_group in enumerate(data):
-    idx = 0
-    idx_other = 1
-    if pairing_group[idx_other][0] in range(pairing_group[idx][0], pairing_group[idx][1]+1):
-        if pairing_group[idx_other][1] in range(pairing_group[idx][0], pairing_group[idx][1]+1):
-            print(range(pairing_group[0][0], pairing_group[0][1]+1), pairing_group[1], end='+\n')
-            running_subset_total += 1
-            continue
-    idx = 1
-    idx_other = 0
-    if pairing_group[idx_other][0] in range(pairing_group[idx][0], pairing_group[idx][1]+1):
-        if pairing_group[idx_other][1] in range(pairing_group[idx][0], pairing_group[idx][1]+1):
-            print(range(pairing_group[1][0], pairing_group[1][1]+1), pairing_group[0], end='-\n')
-            running_subset_total += 1
-def rot():
-    global running_overlap_total
-    print(f"iter={i} groups are subset, added")
-    running_overlap_total +=1     
-for i, pairing_group in enumerate(data):
-    idx = 0
-    idx_other = 1
-    if pairing_group[idx_other][0] in range(pairing_group[idx][0], pairing_group[idx][1]+1):
-        rot()
+range_groups = []
+for pairing_group in data:
+    range_groups.append([range(pairing_group[0][0], pairing_group[0][1]+1), range(pairing_group[1][0], pairing_group[1][1]+1)])
+for item in range_groups:
+    item.sort(key=len, reverse=True)
+for group in range_groups:
+    if group[0] == group[1]:
+        running_subset_total += 1
         continue
-    elif pairing_group[idx_other][1] in range(pairing_group[idx][0], pairing_group[idx][1]+1):
-        rot()
-        continue
-    idx = 1
-    idx_other = 0
-    if pairing_group[idx_other][0] in range(pairing_group[idx][0], pairing_group[idx][1]+1):
-        rot()
-        continue
-    elif pairing_group[idx_other][1] in range(pairing_group[idx][0], pairing_group[idx][1]+1):
-        rot()
-        continue
+    subset_total_counter = len(group[1])
+    for x in group[1]:
+        if x in group[0]:
+            subset_total_counter -= 1
+    if subset_total_counter == 0: running_subset_total += 1
+    print(group[0], group[1], len(group[1]), subset_total_counter)
+for group in range_groups:
+    for x in group[1]:
+        if x in group[0]:
+            running_overlap_total += 1
+            break
 print(running_subset_total, running_overlap_total)
